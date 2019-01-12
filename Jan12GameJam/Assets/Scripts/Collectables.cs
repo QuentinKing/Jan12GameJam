fe7010fc3c1ebdc81a35c574c989cd93b02c3aa9
bill <bill.gan@mail.utoronto.ca>
2019-01-12 13:48:42 -0500

    adding basic collectables and obs

---
+++ Jan12GameJam/Assets/Scripts/Collectables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    public GameObject collectable1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+
+            Instantiate(collectable1, new Vector3(i * 3, i * 3, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void collected()
+    {
+        Destroy(collectable1);
+    }
+}
