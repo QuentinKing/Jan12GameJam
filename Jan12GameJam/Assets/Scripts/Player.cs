84a8434d5f2d62d9f69f51c66615d0e793de395b
TommyX12 <tommyx058@gmail.com>
2019-01-12 12:35:41 -0500

    .

--- Jan12GameJam/Assets/Scripts/Player.cs
+++ Jan12GameJam/Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    private Vector3 up = Vector3(1.0f, 0.0f, 1.0f);
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Movement() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            
+        }
+    }
+
+    void FixedUpdate() {
+        // apply force
+        Movement();
+        // friction
     }
 }
