1b81d8829c58b8640ddadcc243174187ad3c35d3
TommyX12 <tommyx058@gmail.com>
2019-01-12 14:26:45 -0500

    .

--- Jan12GameJam/Assets/Scripts/GameManager.cs
+++ Jan12GameJam/Assets/Scripts/GameManager.cs
@@ -6,14 +6,26 @@ public class GameManager : MonoBehaviour
 {
     public Camera camera;
     protected static GameManager current;
+    public Collectable collectableClass;
     private float totalTime = 120f; // 2 min
     public Player player;
     private float score;
 
-    void Awake()
+    public int numCollectables = 10;
+    public float minX = -8.0f;
+    public float maxX = 8.0f;
+    public float minZ = -8.0f;
+    public float maxZ = 8.0f;
+    public float collectableY = 1.0f;
+
+    protected void Awake()
     {
         GameManager.current = this;
         DontDestroyOnLoad(this);
+    }
+
+    protected void Start() {
+        GenerateCollectables();
     }
 
     public static GameManager GetCurrent()
@@ -21,12 +33,26 @@ public class GameManager : MonoBehaviour
         return GameManager.current;
     }
 
-    void Update()
+    protected void Update()
     {
         totalTime -= Time.deltaTime;
         if (totalTime < 0 || player.GetLives() < 0)
         {
             // UI game over message
+        }
+    }
+
+    public float randRange(float a, float b) {
+        return a + (b - a) * Random.value;
+    }
+
+    public void GenerateCollectables() {
+        for (int i = 0; i < numCollectables; i++) {
+            Instantiate(
+                collectableClass,
+                new Vector3(randRange(minX, maxX), collectableY, randRange(minZ, maxZ)),
+                Quaternion.identity
+            );
         }
     }
 }
