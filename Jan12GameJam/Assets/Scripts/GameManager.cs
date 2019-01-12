e1be37f740d05ba00de2b191278e5de3df4fe6a7
TommyX12 <tommyx058@gmail.com>
2019-01-12 12:24:44 -0500

    .

---
+++ Jan12GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Camera camera;
+
+    protected static GameManager current;
+
+    void Awake() {
+        GameManager.current = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public static GameManager getCurrent() {
+        return GameManager.current;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
