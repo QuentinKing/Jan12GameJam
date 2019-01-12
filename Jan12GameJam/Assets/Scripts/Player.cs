74e51917d603502f162d923b4c7e564a943060c2
TommyX12 <tommyx058@gmail.com>
2019-01-12 13:15:33 -0500

    .

--- Jan12GameJam/Assets/Scripts/Player.cs
+++ Jan12GameJam/Assets/Scripts/Player.cs
@@ -1,26 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     private Rigidbody rb;
-    private Vector3 up = new Vector3(1.0f, 0.0f, 1.0f);
-    private Vector3 right = new Vector3(1.0f, 0.0f, -1.0f);
-    private float thrust = 20.0f;
-    private float friction = 0.9f;
+    public Vector3 up = new Vector3(1.0f, 0.0f, 1.0f);
+    public Vector3 right = new Vector3(1.0f, 0.0f, -1.0f);
+    public float thrust = 20.0f;
+    public float friction = 0.9f;
+    public Vector3 carryDisplacement = new Vector3(0.0f, 3.0f, 0.0f);
+
+    public int initialLives = 5;
+    private int lives;
+
+    public int maxStamina = 5;
+    private int stamina;
+
+    public Rigidbody carryingObject = null;
+    private bool carryingObjectIsKinematic = false;
+    private bool carryingObjectUseGravity = false;
+
+    public Rigidbody GetCarryingObject() {
+        return carryingObject;
+    }
     
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+        lives = initialLives;
+        stamina = maxStamina;
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
     {
         
+    }
+
+    public int GetStamina() {
+        return stamina;
+    }
+
+    public int GetMaxStamina() {
+        return maxStamina;
+    }
+
+    private void UpdateCarryingObject() {
+        if (carryingObject == null) {
+            return;
+        }
+
+        carryingObject.isKinematic = false;
+        carryingObject.position = rb.position + carryDisplacement;
     }
 
     private void Movement() {
@@ -38,14 +74,50 @@ public class Player : MonoBehaviour
         }
     }
 
+    public void StartCarry(Rigidbody obj) {
+        carryingObject = obj;
+        carryingObjectIsKinematic = obj.isKinematic;
+        carryingObjectUseGravity = obj.useGravity;
+        obj.isKinematic = false;
+        obj.useGravity = false;
+    }
+
+    public void Drop() {
+        if (carryingObject != null) {
+            carryingObject.isKinematic = carryingObjectIsKinematic;
+            carryingObject.useGravity = carryingObjectUseGravity;
+            carryingObject = null;
+        }
+    }
+
     private void Friction() {
         Vector3 vel = rb.velocity;
         vel *= friction;
         rb.velocity = vel;
     }
 
-    void FixedUpdate() {
+    public void Die() {
+        throw new NotImplementedException();
+    }
+
+    public void TeleportToBase() {
+        throw new NotImplementedException();
+    }
+
+    public void TakeDamage() {
+        Drop();
+        lives--;
+        if (lives == 0) {
+            
+        }
+        else {
+            TeleportToBase();
+        }
+    }
+
+    protected void FixedUpdate() {
         Movement();
         Friction();
+        UpdateCarryingObject();
     }
 }
